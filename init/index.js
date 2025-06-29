0d1b3675c5b4f418c30667b8406c3d0f24daef23
royalkuriyakosem <royalkuriyakosem@gmail.com>
2025-06-30 01:44:17 +0530

    home page fix

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://admin:1234@cluster0.dgwt0yo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 main()
   .then(() => {
